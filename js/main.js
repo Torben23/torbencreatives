52e33eb00ee70fe403bfe168a520caf9cadd1b79
Torben <105077862+Torben23@users.noreply.github.com>
2025-10-10 15:12:30 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     // Función para ajustar la altura en función de la ventana
     var adjustHeight = function() {
         if ($(window).width() < 992) {
-            $fullheight.css('height', '100%');
+            $fullheight.css('height', '95%');
         } else {
             $fullheight.css('height', $(window).height());
         }
